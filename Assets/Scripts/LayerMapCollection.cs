f4727e8ae2ddfa267d148704b6c4c3eb3db76aad
NickMercer <dekori217@gmail.com>
2023-01-12 07:37:06 -0800

    Basic System Implemented

--- Assets/Scripts/LayerMapCollection.cs
+++ Assets/Scripts/LayerMapCollection.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using Natick.InfluenceMaps;
 using UnityEngine;
 
-namespace Test
+namespace Natick.InfluenceMaps
 {
     internal class LayerMapCollection
     {
